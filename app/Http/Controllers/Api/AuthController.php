dc09eb56c6d0e61b814a6a154d2d6e557a9268da
sergio <sergimonti10@gmail.com>
2024-05-08 17:27:27 +0200

    .

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,9 +4,40 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function login(Request $request)
+    {
+        $response = ['status' => 0, 'message' => 'Invalid Request'];
+        $data = $request->all();
+        $emailOrUsername = $data['email'] ?? null;
+        if ($emailOrUsername === null) {
+            $response['message'] = 'Email or username is required';
+            return response()->json($response);
+        }
+        $user = User::where(function ($query) use ($emailOrUsername) {
+            $query->where('email', $emailOrUsername);
+        })->first();
+        if ($user) {
+            if (Hash::check($data['password'], $user->password)) {
+                $token = $user->createToken('auth_token');
+                $response['status'] = 1;
+                $response['message'] = $token->plainTextToken;
+                $response['user'] = $user;
+                $roles = $user->roles()->pluck('name');
+                $response['roles'] = $roles;
+            } else {
+                $response['message'] = 'Invalid password';
+            }
+        } else {
+            $response['message'] = 'User not found';
+        }
+        return response()->json($response);
+    }
+
     /**
      * Display a listing of the resource.
      */
