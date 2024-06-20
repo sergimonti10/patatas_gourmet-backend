0d25db616d11b3df719ecfbae042f7905424614b
sergio <sergimonti10@gmail.com>
2024-06-20 20:07:41 +0200

    new cors try

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,10 @@
     use Illuminate\Foundation\Configuration\Exceptions;
     use Illuminate\Foundation\Configuration\Middleware;
 
+    header('Access-Control-Allow-Origin: https://patatas-gourmet-frontend-ehv2-pau80ngzr-sergimonti10s-projects.vercel.app');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
     return Application::configure(basePath: dirname(__DIR__))
         ->withRouting(
             web: __DIR__ . '/../routes/web.php',
